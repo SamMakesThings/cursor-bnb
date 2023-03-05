bd269a99b5fdf8bd6c2c34fdb019046188bba250
StartupSam <phantom4d@gmail.com>
2023-03-04 18:32:04 -0800

    Allow unsplash images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['source.unsplash.com', 'images.unsplash.com', 'localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/random/800x800?img=1', // https://source.unsplash.com/random/800x800/?img=1
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/random/800x800?img=1', // https://source.unsplash.com/random/800x800/?img=1
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
